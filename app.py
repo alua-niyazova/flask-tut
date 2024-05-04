8e74866da1263b71e050ae6e91e4ecab440588bd
Alua <alua.niyazova@gmail.com>
2024-05-04 16:51:11 +0000

    added dynamic data and jobs

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {'id' : 1,
+   'title': 'Software Engineer',
+   'location': 'Remote',
+   'salary': '300000tg'
+  },
+  {'id' : 2,
+   'title': 'Front-End Engineer',
+   'location': 'Almaty',
+   'salary': '300000tg'
+  }
+]
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug = True)
